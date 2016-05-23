2fdd743c1ee570f50ca78b774cacdc6a2fd4380e
gkoolstra <gkoolstra@Gerwins-MacBook-Pro.local>
2016-05-23 18:22:33 -0500

    Added functions select_domain and load_maxwell_data

--- trap_analysis.py
+++ trap_analysis.py
@@ -10,12 +10,12 @@ Typical workflow:
 
 """
 from tabulate import tabulate
-from numpy import *
 import numpy as np
 from matplotlib import pyplot as plt
-from slab import dsfit
-from static_surface import load_maxwell_data, select_domain
-import common
+try:
+    from Common import kfit, common
+except:
+    print "Could not load import kfit and common. Please do so manually."
 
 def get_constants():
     """
@@ -24,6 +24,126 @@ def get_constants():
     """
     constants = {'e' : 1.602E-19, 'm_e' : 9.11E-31, 'eps0' : 8.85E-12}
     return constants
+
+def select_domain(X, Y, Esquared, xdomain=None, ydomain=None):
+    """
+    Selects a specific area determined by xdomain and ydomain in X, Y and Esquared. X, Y and Esquared may be
+    obtained from the function load_maxwell_data. To retrieve a meshgrid from the returned 1D arrays x_cut and y_cut,
+    use Xcut, Ycut = meshgrid(x_cut, y_cut)
+    :param X: a 2D array with X coordinates
+    :param Y: a 2D array with Y coordinates
+    :param Esquared: Electric field squared. Needs to be the same shape as X and Y
+    :param xdomain: Tuple specifying the minimum and maximum of the x domain
+    :param ydomain: Tuple specifying the minimum and the maximum of the y domain
+    :return:
+    """
+
+    if xdomain is None:
+        xmin = np.min(X[:,0])
+        xmax = np.max(X[:,0])
+        if ydomain is None:
+            ymin = np.min(Y[0,:])
+            ymax = np.max(Y[0,:])
+    elif ydomain is None:
+        ymin = np.min(Y[0,:])
+        ymax = np.max(Y[0,:])
+    else:
+        xmin, xmax = xdomain
+        ymin, ymax = ydomain
+
+    if np.shape(X) == np.shape(Y) == np.shape(Esquared):
+        if len(np.shape(X)) > 1 and len(np.shape(Y)) > 1:
+            x = X[:,0]
+            y = Y[0,:]
+        else:
+            print "The shape of X and/or Y are not consistent. Aborting. Please Check."
+            return
+
+        x_cut = x[np.logical_and(x>=xmin, x<=xmax)]
+        y_cut = y[np.logical_and(y>=ymin, y<=ymax)]
+
+        xidx = np.where(np.logical_and(x>=xmin, x<=xmax))[0]
+        yidx = np.where(np.logical_and(y>=ymin, y<=ymax))[0]
+
+        Esquared_cut = np.transpose(Esquared[xidx[0]:xidx[-1]+1, yidx[0]:yidx[-1]+1])
+
+        return x_cut, y_cut, Esquared_cut
+    else:
+        print r"Shapes of X, Y and Esquared are not consistent:\nShape X: %d x %d\nShape Y: %d x %d\nShape Esquared: %d x %d "\
+              %(np.shape(X)[0], np.shape(X)[1], np.shape(Y)[0], np.shape(Y)[1], np.shape(Esquared)[0], np.shape(Esquared)[1])
+
+def load_maxwell_data(df, do_plot=True, do_log=True, xlim=None, ylim=None, clim=None,
+                       figsize=(6.,12.), plot_axes='xy', cmap=plt.cm.Spectral):
+    """
+    :param df: Path of the Maxwell data file (fld)
+    :param do_plot: Use pcolormesh to plot the 3D data
+    :param do_log: Plot the log10 of the array. Note that clim has to be adjusted accordingly
+    :param xlim: Dafaults to None. May be any tuple.
+    :param ylim: Defaults to None, May be any tuple.
+    :param clim: Defaults to None, May be any tuple.
+    :param figsize: Tuple of two floats, indicating the figure size for the plot (only if do_plot=True)
+    :param plot_axes: May be any of the following: 'xy' (Default), 'xz' or 'yz'
+    :return:
+    """
+
+    data = np.loadtxt(df, skiprows=2)
+    x = data[:,0]
+    y = data[:,1]
+    z = data[:,2]
+    magE = data[:,3]
+
+    # Determine the shape of the array:
+    if 'x' in plot_axes:
+        for idx, X in enumerate(x):
+            if X != x[0]:
+                ysize=idx
+                xsize=np.shape(magE)[0]/ysize
+                break
+    else:
+        for idx, Y in enumerate(y):
+            if Y != y[0]:
+                ysize=idx
+                xsize=np.shape(magE)[0]/ysize
+                break
+
+    # Cast the voltage data in an array:
+    if plot_axes == 'xy':
+        X = x.reshape((xsize, ysize))
+        Y = y.reshape((xsize, ysize))
+    if plot_axes == 'xz':
+        X = x.reshape((xsize, ysize))
+        Y = z.reshape((xsize, ysize))
+    if plot_axes == 'yz':
+        X = y.reshape((xsize, ysize))
+        Y = z.reshape((xsize, ysize))
+
+    E = magE.reshape((xsize, ysize))
+
+    if do_plot:
+        plt.figure(figsize=figsize)
+        common.configure_axes(15)
+        if do_log:
+            plt.pcolormesh(X*1E6, Y*1E6, np.log10(E), cmap=cmap)
+        else:
+            plt.pcolormesh(X*1E6, Y*1E6, E, cmap=cmap)
+
+        plt.colorbar()
+
+        if clim is not None:
+            plt.clim(clim)
+        if xlim is None:
+            plt.xlim([np.min(x)*1E6, np.max(x)*1E6]);
+        else:
+            plt.xlim(xlim)
+        if ylim is None:
+            plt.ylim([np.min(y)*1E6, np.max(y)*1E6]);
+        else:
+            plt.ylim(ylim)
+        plt.xlabel('x ($\mu\mathrm{m}$)')
+        plt.ylabel('y ($\mu\mathrm{m}$)')
+
+    return X, Y, E
+
 
 def load_potentials(fn_resonator, fn_currentloop, fn_leftgate, fn_midgate, fn_rightgate):
     """
@@ -44,8 +164,8 @@ def load_potentials(fn_resonator, fn_currentloop, fn_leftgate, fn_midgate, fn_ri
 
             plt.title(name)
 
-            output.append({'name' : name, 'V' : array(V, dtype=float64),
-                           'x' : array(x, dtype=float64), 'y' : array(y, dtype=float64)})
+            output.append({'name' : name, 'V' : np.array(V, dtype=np.float64),
+                           'x' : np.array(x, dtype=np.float64), 'y' : np.array(y, dtype=np.float64)})
         else:
             print "Extension not yet supported by this code!"
 
@@ -90,21 +210,21 @@ def fit_electron_potential(x, V, fitdomain=None, do_plot=False, plot_title=''):
     """
     # Fit the potential data to a parabola
     try:
-        fr, ferr = dsfit.fit_parabola(x, V, fitparams=[0, -1, 0], domain=fitdomain, verbose=False)
+        fr, ferr = kfit.fit_parabola(x, V, fitparams=[0, -1, 0], domain=fitdomain, verbose=False)
     except:
         raise ValueError("Fit error!")
 
     if fitdomain is not None:
-        fitdatax, fitdatay = dsfit.selectdomain(x, V, fitdomain)
+        fitdatax, fitdatay = kfit.selectdomain(x, V, fitdomain)
     else:
         fitdatax, fitdatay = x, V
 
     # Get an estimate for the goodness of fit
-    gof = dsfit.get_rsquare(fitdatay, dsfit.parabolafunc(fitdatax, *fr))
+    gof = kfit.get_rsquare(fitdatay, kfit.parabolafunc(fitdatax, *fr))
 
     if do_plot:
         plt.plot(x, V, '.k')
-        plt.plot(fitdatax, dsfit.parabolafunc(fitdatax, *fr), '-r', lw=2.0, label='$r^2$ = %.2f'%(gof))
+        plt.plot(fitdatax, kfit.parabolafunc(fitdatax, *fr), '-r', lw=2.0, label='$r^2$ = %.2f'%(gof))
         plt.xlabel("$x$ ($\mu$m)")
         plt.ylabel("Potential (V)")
         plt.xlim(min(x), max(x))
@@ -124,8 +244,8 @@ def get_electron_frequency(fr, ferr, verbose=True):
     # Calculate the electron frequency; Note that 0.5 ktrap x**2 = a * x**2
     c = get_constants()
 
-    f = 1/(2*pi) * np.sqrt(-c['e']*2*fr[1]*1E12/c['m_e'])
-    sigma_f = 1/(4*pi) * np.sqrt(-2*c['e']*1E12/(fr[1]*c['m_e'])) * ferr[1]
+    f = 1/(2*np.pi) * np.sqrt(-c['e']*2*fr[1]*1E12/c['m_e'])
+    sigma_f = 1/(4*np.pi) * np.sqrt(-2*c['e']*1E12/(fr[1]*c['m_e'])) * ferr[1]
 
     if verbose:
         print "f = %.3f +/- %.3f GHz"%(f/1E9, sigma_f/1E9)
@@ -219,7 +339,7 @@ def sweep_electrode_voltage(x, y, potentials, coefficients, sweep_voltage, sweep
             plt.ylabel('$y$ ($\mu$m)')
 
         # Create a slice along x, where the combined potential is minimized
-        yminidx = np.argmax(V)/shape(V)[1]
+        yminidx = np.argmax(V)/np.shape(V)[1]
         yctridx = len(y)/2
 
         if do_plot:
@@ -228,7 +348,7 @@ def sweep_electrode_voltage(x, y, potentials, coefficients, sweep_voltage, sweep
 
             plt.subplot(132)
 
-        fr, ferr = fit_electron_potential(np.array(x[:]*1E6, dtype=float64), np.array(V[yminidx, :], dtype=float64),
+        fr, ferr = fit_electron_potential(np.array(x[:]*1E6, dtype=np.float64), np.array(V[yminidx, :], dtype=np.float64),
                                           fitdomain=(fitdomain[0]*1E6, fitdomain[1]*1E6), do_plot=do_plot,
                                           plot_title='Minimum found for y = %.2f $\mu$m, V = %.2f V'%(y[yminidx]*1E6, voltage))
 
@@ -242,7 +362,7 @@ def sweep_electrode_voltage(x, y, potentials, coefficients, sweep_voltage, sweep
         if do_plot:
             plt.subplot(133)
 
-        fr, ferr = fit_electron_potential(np.array(x[:]*1E6, dtype=float64), np.array(V[yctridx, :], dtype=float64),
+        fr, ferr = fit_electron_potential(np.array(x[:]*1E6, dtype=np.float64), np.array(V[yctridx, :], dtype=np.float64),
                                           fitdomain=(fitdomain[0]*1E6, fitdomain[1]*1E6), do_plot=do_plot,
                                           plot_title='Center, V = %.2f V'%(voltage))
 
@@ -293,7 +413,7 @@ def get_eigenfreqencies(alpha, beta, f0, P, Q):
     Z = 50.
     L = Z/omega0
 
-    N = common.get_noof_photons_in_cavity(P, omega0/(2*pi), Q)
+    N = common.get_noof_photons_in_cavity(P, omega0/(2*np.pi), Q)
     voltage_scaling = np.sqrt(N) * np.sqrt(1.055E-34 * omega0**2 * Z/2.)
     a1 = alpha * 1E12 # Quadratic component of the DC potential in V/m**2
     beta = voltage_scaling * beta * 1E6 # Linear component of the RF potential in V/m
