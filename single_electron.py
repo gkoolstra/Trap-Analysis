f4ae462b37c294098756101dc86240abb5eb43d8
Gerwin Koolstra <koolstragerwin@gmail.com>
2018-01-24 17:14:37 -0600

    First commit 1/24/18

---
+++ single_electron.py
@@ -0,0 +1,74 @@
+import numpy as np
+from scipy import optimize
+
+def argselectdomain(xdata, domain):
+    ind = np.searchsorted(xdata, domain)
+    return (ind[0], ind[1])
+
+def selectdomain(xdata, ydata, domain):
+    ind = np.searchsorted(xdata, domain)
+    return xdata[ind[0]:ind[1]], ydata[ind[0]:ind[1]]
+
+def voltage_to_frequency(V, V_start, V_crossing, f_cavity):
+    """
+    Converts voltage to frequency
+    :param V:
+    :param Vstart:
+    :param Vcrossing:
+    :param f_cavity:
+    :return:
+    """
+    return f_cavity * np.sqrt(np.abs(V - V_start)) / np.sqrt(V_crossing - V_start)
+
+def susceptibility(g, f_drive, f_electron, gamma):
+    return (2 * np.pi * g) ** 2 / (2 * np.pi * (f_drive - f_electron) + 1j * 2 * np.pi * gamma)
+
+def s21(kappa_tot, g, f_cavity, f_drive, f_electron, gamma):
+    susc = susceptibility(g, f_drive, f_electron, gamma)
+    single_response = 2 * np.pi * kappa_tot / (2 * np.pi * (f_drive - f_cavity) - susc + 1j * 2 * np.pi * kappa_tot)
+
+    phase_s21 = np.arctan2(-np.imag(single_response), np.real(single_response))
+    mag_s21 = np.abs(single_response)
+
+    return mag_s21, phase_s21 - np.pi / 2.
+
+
+
+def fit_phase(voltage, phase, f_cavity, f_drive, V_start, kappa_cavity, fitguess=None, parambounds=None, domain=None, **kwargs):
+
+    if domain is not None:
+        fitdatax, fitdatay = selectdomain(voltage, phase, domain)
+    else:
+        fitdatax = voltage
+        fitdatay = phase
+
+    if parambounds is None:
+        parambounds = (-np.inf, +np.inf)
+
+    # New in scipy 0.17:
+    # * Parameter bounds: constrain fit parameters.
+    #   Example: if there are 3 fit parameters which have to be constrained to (0, inf) we can use
+    #   parambounds = ([0, 0, 0], [np.inf, np.inf, np.inf]), Alternatively, one may set: parambounds = (0, np.inf).
+    #   Default is of course (-np.inf, np.inf)
+
+    def phase_fit_function(voltage, *parameters):
+        [g, gamma, V_crossing] = parameters
+        f_electron = voltage_to_frequency(voltage, Vstart=V_start, Vcrossing=V_crossing, f_cavity=f_cavity)
+        magnitude, phase = s21(kappa_cavity, g, f_cavity, f_drive, f_electron, gamma)
+        return phase * 180 / np.pi
+
+    if fitguess is not None:
+        startparams = fitguess
+    else:
+        # g, gamma, V_crossing
+        startparams = [5E6, 100E6, np.mean(voltage)]
+
+    bestfitparams, covmatrix = optimize.curve_fit(phase_fit_function, fitdatax, fitdatay, startparams, bounds=parambounds, **kwargs)
+
+    try:
+        fitparam_errors = np.sqrt(np.diag(covmatrix))
+    except:
+        print(covmatrix)
+        print("Error encountered in calculating errors on fit parameters. This may result from a very flat parameter space")
+
+    return bestfitparams, fitparam_errors
